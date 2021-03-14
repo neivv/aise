6a1efa2e6a385a233f15c1f8c0757b224c28b642
Markus Heikkinen <ittevien@gmail.com>
2021-03-14 18:26:23 +0200

    Add scr-only feature to bw-dat to make it a bit lighter

--- bw_dat/src/lib.rs
+++ bw_dat/src/lib.rs
@@ -19,7 +19,9 @@ pub mod structs {
 
 use std::mem;
 use std::num::NonZeroU32;
-use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
+use std::sync::atomic::{AtomicUsize, Ordering};
+#[cfg(not(feature = "scr-only"))]
+use std::sync::atomic::AtomicBool;
 
 use bitflags::bitflags;
 
@@ -28,6 +30,7 @@ use serde::{Deserialize, Serialize};
 
 pub use bw::DatTable;
 
+#[cfg(not(feature = "scr-only"))]
 static IS_SCR: AtomicBool = AtomicBool::new(false);
 static BW_MALLOC: AtomicUsize = AtomicUsize::new(0);
 static BW_FREE: AtomicUsize = AtomicUsize::new(0);
@@ -79,12 +82,19 @@ impl ExtendedArray {
     }
 }
 
+#[cfg(not(feature = "scr-only"))]
 pub fn set_is_scr(value: bool) {
     IS_SCR.store(value, Ordering::Relaxed);
 }
 
+#[cfg(not(feature = "scr-only"))]
 fn is_scr() -> bool {
     IS_SCR.load(Ordering::Relaxed) == true
+}
+
+#[cfg(feature = "scr-only")]
+fn is_scr() -> bool {
+    true
 }
 
 pub unsafe fn set_bw_malloc(
