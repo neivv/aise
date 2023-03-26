11799f693264f6a9102a941dd23d2855b934f130
Markus Heikkinen <ittevien@gmail.com>
2023-03-26 21:51:12 +0300

    Fix issuing train fighter order
    As it has special conditions for when it should be reissued.

--- src/unit.rs
+++ src/unit.rs
@@ -133,7 +133,19 @@ impl UnitExt for Unit {
 
     fn issue_secondary_order(self, order: OrderId) {
         unsafe {
-            if (**self).secondary_order != order.0 {
+            // Don't reset the order if already executing the same order, which
+            // seems to be good rule almost always..
+            let mut issue = (**self).secondary_order != order.0;
+            // However, for training fighters the order will stay permamently active in
+            // state 3 / 4 (3 makes carriers heal their interceptors), so it must be allowed
+            // to be reissued unless it is currently in training state (2).
+            if !issue &&
+                order == bw_dat::order::TRAIN_FIGHTER &&
+                (**self).secondary_order_state != 2
+            {
+                issue = true;
+            }
+            if issue {
                 (**self).secondary_order = order.0;
                 (**self).secondary_order_state = 0;
                 // Uhh.. Is this sensible to allow to be done from AI scripts?
